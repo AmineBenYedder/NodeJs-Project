087d0d3e31fe71a2d6a96ef222414069d2f3b8b8
AmineBenYedder <amine.benyedder@yahoo.com>
2024-01-21 20:51:42 +0100

    first commit

---
+++ email-sender.js
@@ -0,0 +1,25 @@
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'node@gmail.com',
+    pass: 'azerty2312',
+  },
+});
+
+const mailOptions = {
+  from: 'node@gmail.com',
+  to: 'amine.benyedder@yahoo.com',
+  subject: 'Test Email',
+  text: 'Hello from Node.js!',
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('Error sending email:', error);
+  } else {
+    console.log('Email sent:', info.response);
+  }
+});
